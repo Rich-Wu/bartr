f1cd908790c8c4f4adbe039b82a0cf6abcfa9f72
Richard <richie1988@gmail.com>
2019-01-28 10:28:49 -0500

    created a route for a new offer

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  get '/main', to: 'pages#main'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#delete'
-  get 'commodity/new'
-  get 'commodity/update'
-  root 'pages#index'
-  get '/main', to: 'pages#main'
   get '/signup', to: 'users#new', as: :signup
   post '/signup', to: 'users#create'
   get '/account', to: 'users#update'
   patch '/account', to: 'users#newinfo'
+  get '/offer/new', to: 'offer#new'
   get '/:username', to: 'users#read'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
