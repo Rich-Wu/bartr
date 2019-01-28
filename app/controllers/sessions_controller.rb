720958dc06e645dfeaf7800eec208c6aff9cf74a
Richard <richie1988@gmail.com>
2019-01-28 09:59:02 -0500

    can login/logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    if User.find_by(email: params[:email])
+      user = User.find_by(email: params[:email])
+      if user.password == params[:password]
+        session[:id] = user.id
+        redirect_to main_path
+      else
+        render 'new'
+      end
+    else
+      render 'new'
+    end
+  end
+
+  def delete
+    session[:id] = nil
+    redirect_to main_path
+  end
+
+end
