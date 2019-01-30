23fa0802fc2160b5cd0546d3ed36ff9526ac6593
Richard <richie1988@gmail.com>
2019-01-30 13:44:17 -0500

    routing logic reworked

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,20 @@
 class PagesController < ApplicationController
+  before_action :verify_login, only: :main
   def index
-
+    if logged_in?
+      redirect_to '/main'
+    end
   end
 
   def main
 
+  end
+
+  private
+
+  def verify_login
+    if !logged_in?
+      redirect_to '/login'
+    end
   end
 end
