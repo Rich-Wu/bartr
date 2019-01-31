9903d79b2c50794437b58904c59d0bb3843b9a63
Richard <richie1988@gmail.com>
2019-01-31 03:46:28 -0500

    added some behavior modifications and styled more of the browse page

--- app/models/commodity.rb
+++ app/models/commodity.rb
@@ -2,4 +2,14 @@ class Commodity < ApplicationRecord
   has_many :offers, dependent: :destroy
   has_many :users, through: :offers
   has_one_attached :image
+  after_commit :add_default_image, on: [:create, :update]
+
+
+  private
+
+  def add_default_image
+    unless image.attached?
+      self.image.attach(io: File.open(Rails.root.join("app", "assets", "images", "default.jpeg")), filename: 'default.jpg' , content_type: "image/jpg")
+    end
+  end
 end
